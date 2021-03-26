4cadaa9c64ef7e353e297854710b07f3cda31721
Mahmoud Arafat <81111174+mahmoud3030arafat@users.noreply.github.com>
2021-03-26 05:07:54 -0700

    Add files via upload

---
+++ searchBar/searchBar/OnceTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  searchBar
+//
+//  Created by Mahmoud on 3/26/21.
+//  Copyright © 2021 mahmoud. All rights reserved.
+//
+
+import UIKit
+
+class OnceTableViewCell: UITableViewCell {
+    
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+
+}
---
+++ searchBar/searchBar/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  searchBar
+//
+//  Created by Mahmoud on 3/26/21.
+//  Copyright © 2021 mahmoud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    let arrayOfText = ["mahmoud","ahmed","ali","sameh","osama","mahmoud","ahmed","ali","sameh","osama"]
+    
+    var searchingArray  = [String]()
+    
+    var searching = false
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableview: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tableview.delegate=self
+        tableview.dataSource=self
+        searchBar.delegate=self
+        print(searching)
+    }
+    
+    
+    
+    // tableview func
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searching {
+        return searchingArray.count
+        }else {
+             return arrayOfText.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell  = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if searching{
+             cell.textLabel?.text=searchingArray[indexPath.row]
+        }else{
+            cell.textLabel?.text=arrayOfText[indexPath.row]
+        }
+        
+        return cell
+        
+    }
+
+
+}
+
+extension ViewController : UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+      searchingArray = arrayOfText.filter({$0.prefix(searchText.count)==searchText})
+        searching = true
+        tableview.reloadData()
+        //
+        print(searchText.count)
+        print(searchText)
+        print(searchingArray)
+        
+    }
+}
+
